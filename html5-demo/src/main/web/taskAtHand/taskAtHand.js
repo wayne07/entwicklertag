c9fab6269d7f3883fe6bb73533297f7bc008156c
Juergen Seliger <juergen.seliger@idos.de>
2015-09-03 10:49:17 +0200

    taskAtHome example 1

---
+++ html5-demo/src/main/web/taskAtHand/taskAtHand.js
@@ -0,0 +1,40 @@
+"use strict";
+function TaskAtHandApp() {
+    var version = "v1.0";
+
+    function setStatus(message) {
+        $("#app>footer").text(message);
+    }
+
+    function addTask() {
+        var taskName = $("#new-task-name").val();
+        if (taskName) {
+            addTaskElement(taskName);
+            // Reset the text field
+            $("#new-task-name").val("").focus();
+        }
+    }
+
+    function addTaskElement(taskName) {
+        var $task = $("<li></li>");
+        $task.text(taskName);
+        $("#task-list").append($task);
+    }
+
+    this.start = function () {
+        $("#new-task-name").keypress(function (e) {
+            if (e.which == 13) { // Enter key
+                addTask();
+                return false;
+            }
+        }).focus();
+
+        $("#app>header").append(version);
+        setStatus("ready");
+    };
+}
+
+$(function () {
+    window.app = new TaskAtHandApp();
+    window.app.start();
+});
